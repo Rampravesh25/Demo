9cd1cd38ef750655ac558792c970471182067ddf
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-06 10:21:02 +0530

    added:Print

--- src/com/bridgelabz/Demo.java
+++ src/com/bridgelabz/Demo.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hi I am Rampravesh");
+
+        System.out.println("Mallah");
     }
 }
